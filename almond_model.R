1e46e9b25449f38096960ec88648a48bf14de210
annaabelman <aabelman@ucsb.edu>
2021-04-08 13:00:30 -0700

    final model?

--- almond_model.R
+++ almond_model.R
@@ -1,14 +1,26 @@
 # enter the almond function here
 
 
-<<<<<<< HEAD
+almond_model <- function(clim, clim_var1 = -0.015, clim_var2 = 0.0046, clim_var3 = 0.07, clim_var4 = 0.0043, intercept = 0.28){
+  
+  temperature <- clim %>% 
+    filter(month == "2") %>% 
+    group_by(year) %>% 
+    summarize(
+      avg = mean(tmin_c)
+    ) 
+  
+  rain <- clim %>% 
+    filter(month == "1") %>% 
+    group_by(year) %>% 
+    summarize(
+      sum = sum(precip)
+    )
+  
+  df <- data.frame(temperature, rain) %>% 
+    select(-year.1)
+  
+  final_model <- clim_var1*(df$avg) - clim_var2*((df$avg)^2) - clim_var3*(df$sum) + clim_var4*((df$sum)^2) + intercept
 
-=======
-almond_model <- function(temp, prep, clim_var1 = -0.015, clim_var2 = 0.0046, clim_var3 = 0.07, clim_var4 = 0.0043, intercept = 0.28){
-  
-  final_model = clim_var1*(temp) - clim_var2*((temp)^2) - clim_var3*(prep) + clim_var4*((prep)^2) + intercept
-  
   return(final_model)
-  
 }
->>>>>>> 9e17d7397b41e65c5195619fd4bdde033493c34d
